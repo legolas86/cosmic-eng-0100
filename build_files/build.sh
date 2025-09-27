4c496bd9dbc69d9e74f9778ff638c7ce33567672
Jason <jason_dick@tcss.us>
2025-09-27 10:13:38 -0400

    added packages for cosmic-de

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,10 +8,12 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
-
+#dnf5 install -y tmux 
+dnf5 group install base-graphical container-management core fonts hardware-support mutlimedia networkmanager-submodules printing development-tools c-development cosmic-desktop -y
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
@@ -19,6 +21,33 @@ dnf5 install -y tmux
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
+dnf install -y bootc \
+                    qemu-kvm \
+                    libvirt \
+                    virt-install \
+                    virt-manager \
+                    toolbox \
+                    distrobox \
+                    flatpak \
+                    tmux \
+                    rust \
+                    cargo \
+                    rustup \
+                    golang \
+                    helix \
+                    code \
+                    bat \
+                    zoxide \
+                    fzf \
+                    tldr \
+                    fd \
+                    btop \
+                    ripgrep \
+
+                                
+
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl set-default graphical.target
+for drv in qemu network nodedev nwfilter secret storage interface; do systemctl start virt${drv}d{,-ro,-admin}.socket; done
