0fcc537f73dbbc9b68bd8d3c501b11c62816ea96
Jason <jason_dick@tcss.us>
2025-09-29 07:32:17 -0400

    changed from fedora bootc to ublue-base-main: latest

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,12 +8,12 @@ set -ouex pipefail
 # RPMfusion repos are available by default in ublue main images
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-#rpm --import https://packages.microsoft.com/keys/microsoft.asc
-#sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 # this installs a package from fedora repos
 #dnf5 install -y tmux 
-dnf group install -y base-graphical container-management core fonts hardware-support multimedia networkmanager-submodules printing development-tools c-development cosmic-desktop
+dnf5 group install -y --nobest base-graphical container-management core fonts hardware-support multimedia networkmanager-submodules printing development-tools c-development cosmic-desktop
 # Use a COPR Example:
 #
 # dnf5 -y copr enable ublue-os/staging
@@ -21,8 +21,8 @@ dnf group install -y base-graphical container-management core fonts hardware-sup
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-dnf -y install tmux bootc qemu-kvm libvirt virt-install virt-manager toolbox distrobox flatpak tmux rust cargo rustup golang helix bat zoxide fzf tldr btop ripgrep rust rustup cargo fish
-
+dnf5 -y install tmux code bootc wireshark qemu-kvm libvirt virt-install virt-manager toolbox distrobox flatpak tmux rust cargo rustup golang helix bat zoxide fzf tldr btop ripgrep rust rustup cargo fish
+dnf5 clean all
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
