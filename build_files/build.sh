12983e27dc5cbebc3fbaef85a02b34ccc2efa9d3
Jason <jason_dick@tcss.us>
2025-10-21 10:43:25 -0400

    added libvirt-work-around and libvirt group

--- build_files/build.sh
+++ build_files/build.sh
@@ -17,7 +17,7 @@ dnf5 -y install ./protonvpn-stable-release-1.0.3-1.noarch.rpm
 dnf5 -y install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 dnf5 -y install terra-release-extras ghostty starship
 dnf5 config-manager setopt "terra*".enabled=0
-
+dnf5 -y copr enable ublue-os/packages
   
 
 
@@ -32,16 +32,17 @@ dnf5 group install -y --nobest base-graphical container-management core fonts ha
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
 
-dnf5 -y install libcap proton-vpn-gnome-desktop tmux code bootc wireshark podmansh tcpdump podman-machine podman-compose podman-tui virt-v2v tiptop qemu-kvm libvirt virt-install virt-manager toolbox distrobox flatpak tmux rust cargo rustup golang helix bat zoxide fzf tldr btop ripgrep rust rustup cargo fish
+dnf5 -y install ublue-os-libvirt-workarounds libcap proton-vpn-gnome-desktop tmux code bootc wireshark podmansh tcpdump podman-machine podman-compose podman-tui virt-v2v tiptop qemu-kvm libvirt virt-install virt-manager toolbox distrobox flatpak tmux rust cargo rustup golang helix bat zoxide fzf tldr btop ripgrep rust rustup cargo fish
+dnf5 -y copr disable ublue-os/packages
 dnf5 clean all
 
 #### Example for enabling a System Unit File
 
 #libvirt group fix
-#grep '^libvirt:' /usr/lib/group | tee -a /etc/group 
+#grep '^libvirt:' /usr/lib/group | tee -a /etc/group
+grep "^$libvirt:" /usr/lib/group | tee -a /etc/group >/dev/null 
 #wireshark fix
 setcap 'cap_net_raw,cap_net_admin+eip' /usr/bin/dumpcap
 
 systemctl enable podman.socket
-systemctl set-default graphical.target
-#for drv in qemu network nodedev nwfilter secret storage interface; do systemctl start virt${drv}d{,-ro,-admin}.socket; done    
+systemctl set-default graphical.target    
